52348afec69cf3dfb199b8ebef1ba4f0858402b2
Haihua Xu <harrison_xu@sphinx.work>
2018-04-01 21:36:27 +0800

    Initial Commit with Express

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+	res.send('OK');
+});
+
+app.listen(3000, function(){
+	console.log('Express is listening on 3000')
+});
+
